db64594dc1e2d5f7619f0ebeba82c65ec6696419
Tahabz <mtahabaz@gmail.com>
2022-04-10 16:55:49 +0000

    refactor: use Promise.all instead of forEach

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const accountSid =
-	process.env.TWILIO_ACCOUNT_SID;
-const authToken =
-	process.env.TWILIO_AUTH_TOKEN;
+const { appendFile } = require("fs").promises;
+const { readFileSync } = require('fs');
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const fromNumber = process.env.FROM;
 
-const { appendFile, readFileSync } = require("fs");
+const client = require("twilio")(accountSid, authToken);
+const message = readFileSync("./message.txt");
+const numbers = readFileSync("./numbers.txt", { encoding: "utf-8" })
+	.split("\n")
+	.filter(Boolean);
 
-const client = require("twilio")(accountSid, authToken)
-
-const message = readFileSync("./message.txt")
-
-readFileSync('./numbers.txt', { encoding: "utf-8" })
-	.split('\n')
-	.forEach(sendMessage)
+Promise.all(numbers.map(sendMessage))
+	.then(logRes)
+	.catch(logError);
 
 function sendMessage(number) {
-	client.messages
-		.create({
-			body: message,
-			from: fromNumber,
-			to: number,
-		})
-		.then((res) => {
-			appendFile("./success-logs", JSON.stringify(res, null, 2) + '\n', logError)
-		})
-		.catch(logError);
+	return client.messages.create({
+		body: message,
+		from: fromNumber,
+		to: number,
+	});
 }
+
+function logRes(res) {
+	return appendFile("./success-logs", JSON.stringify(res, null, 2) + "\n")
+};
 
 function logError(err) {
 	if (err) {
-		console.error(err)
-		appendFile("./error-logs", err + '\n', logError);
+		console.error(err);
+		appendFile("./error-logs", err + "\n");
 	}
 }
